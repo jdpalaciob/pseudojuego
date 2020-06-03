151f228fb9512e2fc14e665f995ce57496363d4b
jdpalaciob <jdpalaciob@gmail.com>
2020-06-03 15:48:26 -0500

    Al "juego de la vida" se le añaden las lineas para dimensionar cada celda y generar una cuadricula sobre el fondo del juego

--- lifegame.py
+++ lifegame.py
@@ -11,6 +11,26 @@ screen = pygame.display.set_mode((width,hight))
 bg = 25, 25, 25     # casi negro, casi oscuro
 screen.fill(bg)     # llenado del fondo
 
+# Celdas
+nxC, nyC = 50, 50   # cantidad de celdas por eje
+dimCW = width // nxC # ancho de cada celda
+dimCH = hight // nyC # alto de cada celda
+
 # Bucle de ejecución
 while True:
-    pass
+
+    # Ciclo para recorrer las celdas generadas
+    for x in range(nxC):
+        for y in range(nyC):
+
+            # Límites dimensionales de los rectángulos
+            poly = [((x) * dimCW, (y) * dimCH),
+            ((x+1) * dimCW, (y) * dimCH),
+            ((x+1) * dimCW, (y+1) * dimCH),
+            ((x) * dimCW, (y+1) * dimCH)]
+
+            # Dibuja los rectángulos. Recibe: pantalla, color, límites de polígonos, grosor
+            pygame.draw.polygon(screen, (128,128,128), poly, 1) # 1 pixel (cuadrícula)
+    
+    # Mostrar los resultados de cuadrícula para cada iteración
+    pygame.display.flip()
