4b153c21a9b9270717c26413662390147db2d2f9
jdpalaciob <jdpalaciob@gmail.com>
2020-06-03 15:04:38 -0500

    Codigo basico para generar el fondo del "juego de la vida"

---
+++ lifegame.py
@@ -0,0 +1,16 @@
+import pygame
+import numpy as np
+
+pygame.init()
+
+# Crear pantalla
+width, hight = 1000,1000    # dimensión en pixeles
+screen = pygame.display.set_mode((width,hight))
+
+# Color de fondo
+bg = 25, 25, 25     # casi negro, casi oscuro
+screen.fill(bg)     # llenado del fondo
+
+# Bucle de ejecución
+while True:
+    pass
