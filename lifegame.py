41466682ff78ffdc452e688e6961e82907ac855e
jdpalaciob <jdpalaciob@gmail.com>
2020-06-03 16:36:14 -0500

    Se crea la estructura de datos (matriz numpy) que almacenara los estados de cada celda. Se añade el escaneo el estado de los vecinos alrededor de una celda. Los cambios aun no afectan el resultado visual.

--- lifegame.py
+++ lifegame.py
@@ -16,6 +16,9 @@ nxC, nyC = 50, 50   # cantidad de celdas por eje
 dimCW = width // nxC # ancho de cada celda
 dimCH = hight // nyC # alto de cada celda
 
+# Estructura de datos para el estado de cada celda
+gameState = np.zeros((nxC, nyC))
+
 # Bucle de ejecución
 while True:
 
@@ -24,10 +27,21 @@ while True:
         for y in range(nyC):
 
             # Límites dimensionales de los rectángulos
-            poly = [((x) * dimCW, (y) * dimCH),
-            ((x+1) * dimCW, (y) * dimCH),
-            ((x+1) * dimCW, (y+1) * dimCH),
-            ((x) * dimCW, (y+1) * dimCH)]
+            poly = [((x)   * dimCW, (y)   * dimCH),
+                    ((x+1) * dimCW, (y)   * dimCH),
+                    ((x+1) * dimCW, (y+1) * dimCH),
+                    ((x)   * dimCW, (y+1) * dimCH)]
+
+            # Número de vecinos cercanos.
+            n_vec = gameState[(x-1) % nxC, (y-1) % nyC] + \
+                    gameState[(x)   % nxC, (y-1) % nyC] + \
+                    gameState[(x+1) % nxC, (y-1) % nyC] + \
+                    gameState[(x-1) % nxC, (y)   % nyC] + \
+                    gameState[(x+1) % nxC, (y)   % nyC] + \
+                    gameState[(x-1) % nxC, (y+1) % nyC] + \
+                    gameState[(x)   % nxC, (y+1) % nyC] + \
+                    gameState[(x+1) % nxC, (y+1) % nyC]
+
 
             # Dibuja los rectángulos. Recibe: pantalla, color, límites de polígonos, grosor
             pygame.draw.polygon(screen, (128,128,128), poly, 1) # 1 pixel (cuadrícula)
