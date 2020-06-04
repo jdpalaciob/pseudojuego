d310ffe164fd028a23c3733dff72aec9442d2322
jdpalaciob <jdpalaciob@gmail.com>
2020-06-04 10:44:19 -0500

    Se añade el evento de oprimir una tecla para pausar el juego (vable. pause). Se modifica el tamaño de la ventana del juego para que encaje a una pantalla de menor resolucion (700 * 700). Se cambia el orden de dibujo de la cuadricula dentro del bucle de ejecucion, de forma que al pausar no sea solo una pantalla negra.

--- lifegame.py
+++ lifegame.py
@@ -5,7 +5,7 @@ import time
 pygame.init()
 
 # Crear pantalla
-width, hight = 1000,1000    # dimensión en pixeles
+width, hight = 700,700    # dimensión en pixeles
 screen = pygame.display.set_mode((width,hight))
 
 # Color de fondo
@@ -33,6 +33,9 @@ gameState[14,15] = 1
 gameState[13,15] = 1
 gameState[12,15] = 1
 
+# Variable de control de ejecución
+pause = False
+
 # Bucle de ejecución
 while True:
 
@@ -46,33 +49,40 @@ while True:
     # Delay para mejor visualización
     time.sleep(0.1)
 
+    # Eventos de teclado
+    ev = pygame.event.get()
+    for event in ev:
+        if event.type == pygame.KEYDOWN:
+            pause = not pause
+
     # Ciclo para recorrer las celdas generadas
     for x in range(nxC):
         for y in range(nyC):
 
+            if not pause:
+
+                # Número de vecinos cercanos.
+                n_vec = gameState[(x-1) % nxC, (y-1) % nyC] + \
+                        gameState[(x)   % nxC, (y-1) % nyC] + \
+                        gameState[(x+1) % nxC, (y-1) % nyC] + \
+                        gameState[(x-1) % nxC, (y)   % nyC] + \
+                        gameState[(x+1) % nxC, (y)   % nyC] + \
+                        gameState[(x-1) % nxC, (y+1) % nyC] + \
+                        gameState[(x)   % nxC, (y+1) % nyC] + \
+                        gameState[(x+1) % nxC, (y+1) % nyC]
+                
+                # REGLA 1: Si muerta y 3 vecinas vivas, entonces revive.
+                if gameState[x, y] == 0 and n_vec == 3:
+                    newState[x, y] = 1
+                # REGLA 2: Si viva y, menos de 2 o más de 3 vivas, entonces muere.
+                elif gameState[x, y] == 1 and (n_vec < 2 or n_vec > 3):
+                    newState[x, y] = 0
+                
             # Límites dimensionales de los rectángulos
             poly = [((x)   * dimCW, (y)   * dimCH),
                     ((x+1) * dimCW, (y)   * dimCH),
                     ((x+1) * dimCW, (y+1) * dimCH),
                     ((x)   * dimCW, (y+1) * dimCH)]
-
-            # Número de vecinos cercanos.
-            n_vec = gameState[(x-1) % nxC, (y-1) % nyC] + \
-                    gameState[(x)   % nxC, (y-1) % nyC] + \
-                    gameState[(x+1) % nxC, (y-1) % nyC] + \
-                    gameState[(x-1) % nxC, (y)   % nyC] + \
-                    gameState[(x+1) % nxC, (y)   % nyC] + \
-                    gameState[(x-1) % nxC, (y+1) % nyC] + \
-                    gameState[(x)   % nxC, (y+1) % nyC] + \
-                    gameState[(x+1) % nxC, (y+1) % nyC]
-            
-            # REGLA 1: Si muerta y 3 vecinas vivas, entonces revive.
-            if gameState[x, y] == 0 and n_vec == 3:
-                newState[x, y] = 1
-            # REGLA 2: Si viva y, menos de 2 o más de 3 vivas, entonces muere.
-            elif gameState[x, y] == 1 and (n_vec < 2 or n_vec > 3):
-                newState[x, y] = 0
-
 
             # Dibuja los rectángulos. Recibe: pantalla, color, límites de polígonos, grosor
             if gameState[x, y] == 0:
