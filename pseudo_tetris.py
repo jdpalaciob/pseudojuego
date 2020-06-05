946eef4012fcfff35dae0f0031a01b143c9f207f
jdpalaciob <jdpalaciob@gmail.com>
2020-06-05 15:49:53 -0500

    Primera version de la interfaz de nustro pseudojuego. Inspiracion en "The Wall" the pink floyd, puesto que hay que evitar la construccion de un muro.

---
+++ pseudo_tetris.py
@@ -0,0 +1,22 @@
+""" JUEGO TRIBUTO A TETRIS """
+
+import pygame
+
+pygame.init()
+
+#### INTERFACE ####
+# Screen #
+width, height = 700, 700
+screen = pygame.display.set_mode((width, height))
+# Header #
+icono = pygame.image.load('icono.jpeg')
+pygame.display.set_icon(icono)
+pygame.display.set_caption('PseudoTetris')
+# Background #
+screen.fill((25, 25, 25))
+fondo = pygame.image.load('fondo.jpg').convert()
+
+### EXECUTION LOOP ####
+while True:
+    screen.blit(fondo, (58, 0))
+    pygame.display.flip()
