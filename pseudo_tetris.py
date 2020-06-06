96ffdaa9a6daa9242775c1c4d995cd0305faea1d
luis ramirez <laramirezt@unal.edu.co>
2020-06-06 16:38:47 -0500

    Se agregan las funciones para iniciar y parar la musica, teniendo parametrizado el volumen que va de 0 a 1 en valores decimales.

--- pseudo_tetris.py
+++ pseudo_tetris.py
@@ -2,6 +2,20 @@
 
 import pygame
 
+# Functions
+
+
+def play_music(path_track, volume, loop=1000):
+    pygame.mixer.init()
+    pygame.mixer.music.set_volume(volume)
+    pygame.mixer.music.load(path_track)
+    pygame.mixer.music.play(loops=loop)
+
+
+def stop_music():
+    pygame.mixer.music.stop()
+
+
 pygame.init()
 
 #### INTERFACE ####
@@ -16,6 +30,9 @@ pygame.display.set_caption('PseudoTetris')
 screen.fill((25, 25, 25))
 fondo = pygame.image.load('fondo.jpg').convert()
 
+# Add music to the window
+play_music("sources/music/the_wall_theme.mp3", 0.8)
+
 ### EXECUTION LOOP ####
 RUN = True
 while RUN:
@@ -26,3 +43,5 @@ while RUN:
 
     screen.blit(fondo, (58, 0))
     pygame.display.flip()
+
+
